721b80a384d43300451183228a80fe42ff2477f1
Harishchandu12 <148865184+Harishchandu12@users.noreply.github.com>
2024-12-26 09:41:39 +0000

    Create BFridaySalesPredictions_Scenario_1.py

---
+++ final_code/BFridaySalesPredictions_Scenario_1.py
@@ -0,0 +1,29 @@
+
+
+# import dependencies/libraries  
+
+import numpy as np  # Array Operations and Mathematical Operations
+import pandas as pd  # Analyzing and manipulating the data, especially for DataFrames
+import seaborn as sb  # to visualize random distributions/statistical graphics
+from sklearn.impute import SimpleImputer # Statistical data visualization and plotting
+import matplotlib # support for data exploration through visualization
+import matplotlib.pyplot as plt # For plotting graphs and visualizations 
+import sklearn # Importing the main scikit-learn library for machine learning functions
+import os
+
+
+# Get the current directory path
+current_directory = os.getcwd()
+print(f"Current Directory: {current_directory}")
+
+
+#  Dataset Loading
+file_name = 'train.csv'
+file_path = os.path.join(current_directory, file_name)
+
+# Load the dataset into a DataFrame
+bfriday_sales_train_df = pd.read_csv(file_path)
+print("The train dataset has been loaded")
+
+#displays the initial data of train data
+print(bfriday_sales_train_df.head(10))
