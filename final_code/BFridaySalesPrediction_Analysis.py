7b18d043dada3d13cbc013996985826705c41504
Harishchandu12 <148865184+Harishchandu12@users.noreply.github.com>
2024-12-27 19:07:03 +0000

    Update BFridaySalesPrediction_Analysis.py

--- final_code/BFridaySalesPrediction_Analysis.py
+++ final_code/BFridaySalesPrediction_Analysis.py
@@ -1,51 +1,6 @@
 #  Title: Black Friady Sales Prediction Using Machine Learning.
 
-'''
-The goal of this project is to identify the best regression model for predicting Black Friday sales. 
-The first step involved thoroughly examining the data to understand its structure and the various features it contains. 
-I performed data cleaning to ensure there were no inconsistencies, such as missing values, and filled in any gaps using appropriate methods, 
-like filling missing values with the mean or null values where needed. Additionally, I checked the descriptive statistics of the data to get a better 
-sense of its distribution and identify any unusual patterns.
-
-Next, I conducted an Exploratory Data Analysis (EDA) to gain deeper insights into the dataset. 
-This involved analyzing both numerical and categorical variables to understand their distributions, trends, and any potential outliers. 
-I also looked into the relationships between the independent features (such as user demographics, product details, etc.) and the target variable, 
-which in this case is the 'purchase' amount. Based on the findings from EDA, I enhanced the dataset by creating new features or modifying existing ones 
-to improve model performance. This included feature encoding for categorical variables and feature scaling to ensure that the data was ready for machine learning
-models.
-
-After preparing the data, I split the dataset into training and testing sets. 
-I then tested five different regression models—Linear Regression, XGBoost Regression and Random Forest Regression. 
-The goal was to compare their performance and identify the model that would provide the most accurate predictions for Black Friday sales. 
-Each model was evaluated based on key metrics like Mean Absolute Error (MAE), Root Mean Squared Error (RMSE), and R² score, and the best-performing model 
-was selected for further use. This approach allowed me to explore various regression techniques and determine the most suitable model for the task at hand.
-
-'''
-
-#  Dataset Description 
-
-'''
-  * Dataset Source: Kaggle (Black Friday Sales)
-  * blackfriday_dataset.csv: The model and purchase prediction will be built using this file(Target variable)
-  * The data set consists of following Columns:
-  * User_ID : User id of the customer
-  * Product_ID: Product id of the product
-  * Gender: male or female
-  * Age: Age in bins i.e 0-17, 18-25, 26-35, 36-45, 46-50, 51-55, 55+
-  * Occupation: Occupation (Masked)
-  * City_Category: Category of the City (A,B,C)
-  * Stay_In_Current_City_Years: Total number of years stay in current city
-  * Marital_Status: 0-Unmarried, 1-Married
-  * Product_Category_1: Product Category (Masked)
-  * Product_Category_2: Product may belongs to other category also (Masked)
-  * Product_Category_3: Product may belongs to other category also (Masked)
-  * Purchase: Purchase Amount (Target Variable)
-
-'''
-
-
 # import dependencies/libraries  
-
 
 import numpy as np  # Array Operations and Mathematical Operations
 import pandas as pd  # Analyzing and manipulating the data, especially for DataFrames
@@ -170,5 +125,43 @@ Unique_User_IDs = len(set(bfriday_sales_train_df.User_ID))
 Total_User_IDs = bfriday_sales_train_df.shape[0]
 Dup_User_IDs = Total_User_IDs - Unique_User_IDs
 print("There are " + str(Dup_User_IDs) + " duplicate User_ID for " + str(Total_User_IDs) + " total number of transactions in the dataset")
+
+
+# 3. Outlier Analysis
+
+#Diplay 25% and 75% Quartile values
+Q1= bfriday_sales_train_df["Purchase"].quantile(0.25)
+Q3= bfriday_sales_train_df["Purchase"].quantile(0.75)
+print(Q1,Q3)
+
+
+# Display Inter quartile range
+IQR = Q3 - Q1
+print(IQR)
+
+
+# Display the outlier with 1.5 standard range
+lower_lmt = Q1 - 1.5 *IQR
+upper_lmt = Q3 + 1.5 *IQR
+
+print("Upper limit of outlier is: ",upper_lmt)
+print("Lower limit of outlier is: ",lower_lmt)
+
+
+# Display the outliers purchase max and min values
+outliers_df=bfriday_sales_train_df[(bfriday_sales_train_df.Purchase < lower_lmt)|(bfriday_sales_train_df.Purchase > upper_lmt)]
+print("number of outliers: ",outliers_df['Purchase'].count())
+print("max purcahse value from the outliers is: ",outliers_df['Purchase'].max())
+print("min purchase value from the outliers: ",outliers_df['Purchase'].min())
+
+
+# to show the outliers in the purchases using boxplot
+sb.boxplot(data=bfriday_sales_train_df, x="Purchase").set(title='Boxplot of Purchase')
+
+
+# 4. Skewness and Distribution Checks
+
+# Display the skew data from the dataset
+print(bfriday_sales_train_df['Purchase'].skew())
 
 
